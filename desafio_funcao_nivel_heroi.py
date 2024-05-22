8d9eeeb36566637080e5fa4de1a0155c41ebdf40
Eduarda Reis <reis3332@gmail.com>
2024-05-22 00:36:19 -0300

    upgrade desafio que usa funções

--- desafio_funcao_nivel_heroi.py
+++ desafio_funcao_nivel_heroi.py
@@ -1,27 +1,27 @@
 def calcular_nivel_rank(vitorias, derrotas):
-    
-    saldoVitorias = vitorias - derrotas
+  
+  saldoRank = vitorias - derrotas
 
-    if vitorias <= 10:
-        nivel = "Ferro"
-    elif 11 <= vitorias <= 20:
-        nivel = "Bronze"
-    elif 21 <= vitorias <= 50:
-        nivel = "Prata"
-    elif 51 <= vitorias <= 80:
-        nivel = "Ouro"
-    elif 81 <= vitorias <= 90:
-        nivel = "Diamante"
-    elif 91 <= vitorias <= 100:
-        nivel = "Lendário"
-    else:
-        nivel = "Imortal"
-
-    return saldoVitorias, nivel
+  if saldoRank <= 10:
+    nivel = "Ferro"
+  elif 11 <= saldoRank <= 20:
+    nivel = "Bronze"
+  elif 21 <= saldoRank <= 50:
+    nivel = "Prata"
+  elif 51 <= saldoRank <= 80:
+    nivel = "Ouro"
+  elif 81 <= saldoRank <= 90:
+    nivel = "Diamante"
+  elif 91 <= saldoRank <= 100:
+    nivel = "Lendário"
+  else:
+    nivel = "Imortal"
+  
+  return saldoRank, nivel
 
 vitorias = int(input("Digite a quantidade de vitórias do jogador: "))
 derrotas = int(input("Digite a quantidade de derrotas do jogador: "))
 
-saldoVitorias, nivel = calcular_nivel_rank(vitorias, derrotas)
+saldoRank, nivel = calcular_nivel_rank(vitorias, derrotas)
 
-print(f"O Herói tem um saldo de {saldoVitorias}, está no nível de {nivel}.")
+print(f"O Herói tem um saldo de {saldoRank}, e está no nível de {nivel}.")
